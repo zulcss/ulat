d1fae73e2bc9d12c789b315f5dceef07f77f3ccb
Charles Short <charles.short@windriver.com>
2023-04-16 10:21:46 -0400

    ostree build #3
    Signed-off-by: Charles Short <charles.short@windriver.com>

--- pkg/compose/ostree.go
+++ pkg/compose/ostree.go
@@ -1,9 +1,27 @@
 package compose
 
+import (
+	"log"
+)
+
 type Ostree struct {
-	Repo string
+	RootfsDir string
 }
 
 func OstreeCreate(c *Compose) error {
+	log.Println("Creating ostree repo...")
+
+	return nil
+}
+
+func (o *Ostree) Unpack(c *Compose) error {
+	log.Println("Unpacking tarball...")
+
+	return nil
+}
+
+func (o *Ostree) BuildBranch(c *Compose) error {
+	log.Println("Building ostree branch...")
+
 	return nil
 }
